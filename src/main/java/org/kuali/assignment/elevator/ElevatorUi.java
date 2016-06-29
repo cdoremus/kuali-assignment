2e27d0e5345027a89955a3950319cba071d91858
Craig Doremus <cdoremus@gmail.com>
2016-06-29 12:24:46 -0400

    Stubbed out possible UI for the simulation

--- src/main/java/org/kuali/assignment/elevator/ElevatorUi.java
+++ src/main/java/org/kuali/assignment/elevator/ElevatorUi.java
@@ -2,12 +2,30 @@ package org.kuali.assignment.elevator;
 
 public class ElevatorUi {
 	
+	private ElevatorRequestController requestController;
+	
+	public ElevatorUi() {
+		requestController = new ElevatorRequestController();
+	}
+	
+	public void callElevator(int startFloor, int destinationFloor) {
+		requestController.call(new ElevatorCallState(startFloor, destinationFloor));
+	}
 	
 	public static void main(String[] args) {
 		//initialize system
 		ElevatorSystemInitializer initializer = new ElevatorSystemInitializer();
 		initializer.init();
 		
+		//FIXME: This is not complete. The simulation can be seen by running the ElevatorRequestControllerTest test class.
+		
+		//TODO: There would need to be a loop here to go through simulation with N number of call requests
+		//	and travels to floor destination
+
+		//TODO: Example of the initialization of a instance of an elevator request and trip from a given start floor and destination floor  
+		ElevatorUi ui = new ElevatorUi();
+		ui.callElevator(1, 6);
+
 	}
 
 }
